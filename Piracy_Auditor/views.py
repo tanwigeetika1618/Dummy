b7783e8e4ebfb1dce2054bf6740c00f875e73b5c
tanwigeetika1618 <84617407+tanwigeetika1618@users.noreply.github.com>
2023-08-08 22:06:03 +0530

    Initial Commit

---
+++ Piracy_Auditor/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render 
+def home(request):
+    return render(request,'index.html')
+#def next1(request):
+   # return render(request,'next1.html')
+def text(request):
+    return render(request,'text.html')
+def file(request):
+    pass
+def compare(request):
+    pass
+def result(request):
+    pass
+def fileTestResult(request):
+    pass
+def textSimilarityAnalysis(request):
+    pass
+def fileSimilarityAnalysis(request):
+    pass
+
